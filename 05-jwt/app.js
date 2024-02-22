d5ea5856e460a35647d4ea4b1a86d8d7beaa9ee2
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2024-02-22 15:53:59 +0530

    jwt basics

--- 05-jwt/app.js
+++ 05-jwt/app.js
@@ -2,27 +2,46 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 // view engine
 app.set("view engine", "ejs");
 
-// database connection
-// const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-//   .then((result) => app.listen(3000))
-//   .catch((err) => console.log(err));
+//database connection
+
+const dbURI =
+  "mongodb+srv://partiktanwar30402:qwert12@task-manager.nniccqt.mongodb.net/";
 mongoose
-  .connect(
-    "mongodb+srv://partiktanwar30402:yTRUM74QVWUvizBk@task-manager.nniccqt.mongodb.net/node-auth",
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  )
-  .then(() => app.listen(3000))
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
+app.get("*", checkUser);
 app.get("/", (req, res) => res.render("home"));
-app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+// app.get("/set-cookies", (req, res) => {
+//   //   res.setHeader("Set-Cookie", "newUser=true");
+//   res.cookie("newUser", false);
+
+//   res.cookie("isEmployee", true, { maxAge: 1000 * 60 * 60, httpOnly: true });
+
+//   res.send("you have got the cookies");
+// });
+
+// app.get("/read-cookies", (req, res) => {
+//   const cookies = req.cookies;
+//   console.log(cookies);
+//   res.json(cookies);
+// });
